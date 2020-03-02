fc62a355e00ed5b9d823644808e47267db15b53a
sandy ielegems <sandy.ielegems@outlook.com>
2020-03-02 11:21:22 +0100

    oef 2

---
+++ 03-wk2018.js
@@ -0,0 +1,6 @@
+function toonWinnaars() {
+    var landen = ['Frankrijk', 'Kroatië', 'België'];
+    document.write('Op plaats 1 staat :' + landen[0]);
+    document.write('Op plaats 2 staat :' + landen[1]);
+    document.write('Op plaats 3 staat :' + landen[2]);
+}
