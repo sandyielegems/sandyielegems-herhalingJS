905bb2d5a1c171c6cabb45e648d99ee5fd96b67b
sandy ielegems <sandy.ielegems@outlook.com>
2020-03-02 13:18:31 +0100

    oef 4

--- 03-wk2018.js
+++ 03-wk2018.js
@@ -1,9 +1,7 @@
 function toonWinnaars() {
-    var landen = new Array();
-    landen[0] = 'Frankrijk';
-    landen[1] = 'Kroatië';
-    landen[2] = 'België';
+    var landen = ['Frankrijk', 'Kroatië', 'België'];
     document.write('Op plaats 1 staat : ' + landen[0]);
     document.write('<p>Op plaats 2 staat : ' + landen[1] + '</p>');
     document.write('<p>Op plaats 3 staat : ' + landen[2] + '</p>');
 }
+toonWinnaars();
